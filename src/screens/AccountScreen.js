2e5ba445ae013ace7546357cdbf5788da1273104
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-28 20:57:16 +0530

    Create boilerplate and add the navigation flows for all screens

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const AccountScreen = () => {
+    return (
+        <View>
+            <Text> AccountScreen </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default AccountScreen;
