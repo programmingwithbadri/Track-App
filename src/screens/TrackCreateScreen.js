49a342c221df5f64eda00bfabf7c9420deb0d72b
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-30 23:10:13 +0530

    Extract code and create useLocation hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,35 +1,15 @@
 import '../_mockLocation';
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/Map'
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'
 import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
     const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10,
-            }, (location) => {
-                addLocation(location);
-            });
-        } catch (e) {
-            console.error(e)
-            setErr(e);
-        }
-    }
-
-    useEffect(() => {
-        startWatching();
-    }, [])
-
+    const [err] = useLocation(addLocation);
     return (
         <SafeAreaView>
             <Text h2> TrackCreateScreen </Text>
