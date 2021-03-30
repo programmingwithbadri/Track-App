03c9d558b75b242ff555543ad3b7780260e6a133
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-30 16:56:06 +0530

    Ask for location access permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/Map'
+import { requestPermissionsAsync } from 'expo-location'
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+        } catch (e) {
+            setErr(e);
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    }, [])
+
     return (
         <SafeAreaView>
             <Text h2> TrackCreateScreen </Text>
             <Map />
+            {err
+                ? <Text>PLease enable the location</Text>
+                : null
+            }
         </SafeAreaView>
     )
 }
