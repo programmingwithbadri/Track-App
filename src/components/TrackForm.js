6cd6c7a51c93ad875003ed6c8c49293b6079ca1c
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-31 20:06:44 +0530

    Create the useSaveTrack hook to coordinate different contexts

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,12 +2,15 @@ import React, { useContext } from 'react'
 import { Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack'
 
 const TrackForm = () => {
     const { state: { name, recording, locations },
         startRecording,
         stopRecording,
         changeName } = useContext(LocationContext);
+
+    const [saveTrack] = useSaveTrack();
 
     return (
         <>
@@ -21,7 +24,7 @@ const TrackForm = () => {
             <Spacer>
                 {
                     !recording && locations.length
-                        ? <Button title="Save the recording" />
+                        ? <Button title="Save the recording" onPress={saveTrack} />
                         : null
                 }
             </Spacer>
