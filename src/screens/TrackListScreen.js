8f80af5874482b56d9760cdb8072142b4ea7d4c2
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-31 23:07:31 +0530

    Retrieve tracks and show track details

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useContext } from 'react'
+import { StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import { Context as TrackContext } from '../context/TrackContext';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext);
     return (
-        <View>
-            <Text> TrackListScreen </Text>
-        </View>
+        <>
+            <NavigationEvents onWillFocus={fetchTracks} />
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate('TrackDetail', { _id: item._id });
+                        }}>
+                            <ListItem bottomDivider >
+                                <ListItem.Content>
+                                    <ListItem.Title>{item.name}</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron />
+                            </ListItem>
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+        </>
     )
 }
 
